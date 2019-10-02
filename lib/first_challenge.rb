c3d40f07b627b0870a061d4f750f2e11c0b95ba8
Lenny Lee <lennylee91@gmail.com>
2019-10-02 00:20:39 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,21 +1,8 @@
 def first_challenge
-   epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-      }, 
-    :capulet => {
-      #your key/value pairs here!
-      :patriarch => {},
-      :matriarch => {},
-      :heroine => {},
-      :heroine_friends => []
-
-      }
-  }
+ epic_tragedy = {
+  :montague => {},
+  :capulet => {}
+}
 
 
 end
